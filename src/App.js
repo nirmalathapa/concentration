eb18c305f6d76b3320abe07446b4e555d24d1151
Rajesh Sharma <rajesh@surf.net.np>
2018-10-03 20:40:19 +0200

    Fix lint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, { Component } from "react";
-import "./App.css";
-import img1 from "./img1.jpg";
-import img2 from "./img2.jpg";
-import img3 from "./img3.jpg";
-import img4 from "./img4.jpg";
-import img5 from "./img5.jpg";
+import React, { Component } from 'react'
+import './App.css'
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+import img5 from './img5.jpg'
 
 class Card extends Component {
   render() {
     return (
       <div className="card" onClick={this.props.onClick}>
-        {this.props.flipped ? this.props.children : <p>Not flipped</p>}        
+        {this.props.flipped ? this.props.children : <p>Not flipped</p>}
       </div>
-    );
+    )
   }
 }
 
 class App extends Component {
   state = {
-    1: false, 
+    1: false,
     2: false,
     3: false,
     4: false,
@@ -28,7 +28,7 @@ class App extends Component {
     8: false,
     9: false,
     10: false
-  };
+  }
 
   reset = () => {
     this.setState({
@@ -42,21 +42,20 @@ class App extends Component {
       8: false,
       9: false,
       10: false
-    });
-  };
+    })
+  }
 
-  isFlipped = id => this.state[id];
-  
+  isFlipped = id => this.state[id]
 
   toggle = id => {
-      if(Object.values(this.state).filter(i => i === true).length >= 2) {
-          return this.reset()
-      }
+    if (Object.values(this.state).filter(i => i === true).length >= 2) {
+      return this.reset()
+    }
 
-    this.setState({        
-        ...this.state,
-        [id]: !this.isFlipped(id)
-      });  
+    this.setState({
+      ...this.state,
+      [id]: !this.isFlipped(id)
+    })
   }
 
   render() {
@@ -66,41 +65,81 @@ class App extends Component {
           <button onClick={() => this.reset()}>Reset Game</button>
           <button>Score</button>
           <div className="wrap">
-            <Card flipped={this.isFlipped(1)} onClick={() => this.toggle(1)} IndexNum={1}>
+            <Card
+              flipped={this.isFlipped(1)}
+              onClick={() => this.toggle(1)}
+              IndexNum={1}
+            >
               <img src={img1} alt="img" />
             </Card>
-            <Card flipped={this.isFlipped(2)} onClick={() => this.toggle(2)} IndexNum={2}>
+            <Card
+              flipped={this.isFlipped(2)}
+              onClick={() => this.toggle(2)}
+              IndexNum={2}
+            >
               <img src={img2} alt="img" />
             </Card>
-            <Card flipped={this.isFlipped(3)} onClick={() => this.toggle(3)} IndexNum={3}>
+            <Card
+              flipped={this.isFlipped(3)}
+              onClick={() => this.toggle(3)}
+              IndexNum={3}
+            >
               <img src={img3} alt="img" />
             </Card>
-            <Card flipped={this.isFlipped(4)} onClick={() => this.toggle(4)} IndexNum={4}>
+            <Card
+              flipped={this.isFlipped(4)}
+              onClick={() => this.toggle(4)}
+              IndexNum={4}
+            >
               <img src={img4} alt="img" />
             </Card>
-            <Card flipped={this.isFlipped(5)} onClick={() => this.toggle(5)} IndexNum={5}>
+            <Card
+              flipped={this.isFlipped(5)}
+              onClick={() => this.toggle(5)}
+              IndexNum={5}
+            >
               <img src={img5} alt="img" />
             </Card>
-            <Card flipped={this.isFlipped(6)} onClick={() => this.toggle(6)} IndexNum={1}>
+            <Card
+              flipped={this.isFlipped(6)}
+              onClick={() => this.toggle(6)}
+              IndexNum={1}
+            >
               <img src={img1} alt="img" />
             </Card>
-            <Card flipped={this.isFlipped(7)} onClick={() => this.toggle(7)} IndexNum={2}>
+            <Card
+              flipped={this.isFlipped(7)}
+              onClick={() => this.toggle(7)}
+              IndexNum={2}
+            >
               <img src={img2} alt="img" />
             </Card>
-            <Card flipped={this.isFlipped(8)} onClick={() => this.toggle(8)} IndexNum={3}>
+            <Card
+              flipped={this.isFlipped(8)}
+              onClick={() => this.toggle(8)}
+              IndexNum={3}
+            >
               <img src={img3} alt="img" />
             </Card>
-            <Card flipped={this.isFlipped(9)} onClick={() => this.toggle(9)} IndexNum={4}>
+            <Card
+              flipped={this.isFlipped(9)}
+              onClick={() => this.toggle(9)}
+              IndexNum={4}
+            >
               <img src={img4} alt="img" />
             </Card>
-            <Card flipped={this.isFlipped(10)} onClick={() => this.toggle(10)} IndexNum={5}>
+            <Card
+              flipped={this.isFlipped(10)}
+              onClick={() => this.toggle(10)}
+              IndexNum={5}
+            >
               <img src={img5} alt="img" />
             </Card>
           </div>
         </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
